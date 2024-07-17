423243f4e2df588f94575304351c559e7233c3d2
d_nekh <dnekh.dev@gmail.com>
2024-07-17 19:50:21 +0900

    Initialized game field and set screen size.

--- src/com/javarush/task/jdk13/task53/task5303/SnakeGame.java
+++ src/com/javarush/task/jdk13/task53/task5303/SnakeGame.java
@@ -1,5 +1,13 @@
 package com.javarush.task.jdk13.task53.task5303;
 
-public class SnakeGame {
+import com.javarush.engine.cell.*;
 
+public class SnakeGame extends Game {
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+    }
 }
