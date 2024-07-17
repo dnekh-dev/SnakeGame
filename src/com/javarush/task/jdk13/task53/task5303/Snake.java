eb965865059ef1e105be532d27885db4822b01b3
d_nekh <dnekh.dev@gmail.com>
2024-07-17 21:11:45 +0900

    In Snake class were added fields and draw method for drawing a snake.

--- src/com/javarush/task/jdk13/task53/task5303/Snake.java
+++ src/com/javarush/task/jdk13/task53/task5303/Snake.java
@@ -1,16 +1,30 @@
 package com.javarush.task.jdk13.task53.task5303;
 
+import com.javarush.engine.cell.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Snake extends GameObject {
 
     private List<GameObject> snakeParts = new ArrayList<>();
+    private static final String HEAD_SIGN = "\uD83D\uDC7E";
+    private static final String BODY_SIGN = "\u26AB";
 
     public Snake(int x, int y) {
         super(x, y);
         snakeParts.add(new GameObject(x, y));
         snakeParts.add(new GameObject(x + 1, y));
         snakeParts.add(new GameObject(x + 2, y));
+    }
+
+    public void draw(Game game) {
+        for (int i = 0; i < snakeParts.size(); i++) {
+            if (i == 0) {
+                game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, HEAD_SIGN);
+            } else {
+                game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, BODY_SIGN);
+            }
+        }
     }
 }
--- src/com/javarush/task/jdk13/task53/task5303/SnakeGame.java
+++ src/com/javarush/task/jdk13/task53/task5303/SnakeGame.java
@@ -5,6 +5,7 @@ import com.javarush.engine.cell.*;
 public class SnakeGame extends Game {
     public static final int WIDTH = 15;
     public static final int HEIGHT = 15;
+    private Snake snake;
 
     @Override
     public void initialize() {
@@ -13,6 +14,7 @@ public class SnakeGame extends Game {
     }
 
     private void createGame() {
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
         drawScene();
         /*Apple apple = new Apple(7, 7);
         apple.draw(this);*/
@@ -24,5 +26,6 @@ public class SnakeGame extends Game {
                 setCellColor(x, y, Color.GREY);
             }
         }
+        snake.draw(this);
     }
 }
